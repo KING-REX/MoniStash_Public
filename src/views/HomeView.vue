c4ab53cd5a40a5ef7d00a7f2050572ef8e56ac9d
KING-REX <anijimjamesmaurice@gmail.com>
2024-03-18 22:05:02 +0100

    Rex's Commit: Adding all the sections into the HomeView page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,39 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import FeatureOne from '@/components/FeatureOne.vue'
+import HomeHeader from '@/components/HomeHeader.vue'
+import ExchangeRates from '@/components/ExchangeRates.vue'
+import WhySave from '@/components/WhySave.vue'
+import FeatureTwo from '@/components/FeatureTwo.vue'
+import HowItWorks from '@/components/HowItWorks.vue'
+import AppDownload from '@/components/AppDownload.vue'
+import AppFooter from '@/components/AppFooter.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <HomeHeader />
+    <main>
+        <hr />
+        <FeatureOne />
+        <hr />
+        <ExchangeRates />
+        <hr />
+        <WhySave />
+        <hr />
+        <FeatureTwo />
+        <hr />
+        <HowItWorks />
+        <hr />
+        <AppDownload />
+        <hr :style="{ backgroundColor: '#fff' }" />
+    </main>
+    <AppFooter />
 </template>
+
+<style scoped>
+hr {
+    height: 5px;
+    background-color: #fafafa;
+    outline: none;
+    border: none;
+}
+</style>
