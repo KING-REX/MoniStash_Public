8f0aa0d394445536cef09603de8db06fbb0055c6
KING-REX <anijimjamesmaurice@gmail.com>
2024-03-21 13:51:56 +0100

    Rex's Commit: Making the Footer an app-level component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HomeHeader from './components/HomeHeader.vue'
 import NavBar from './components/NavBar.vue'
+import AppFooter from './components/AppFooter.vue'
 </script>
 
 <template>
     <NavBar />
     <RouterView />
+    <AppFooter />
 </template>
 
 <style scoped></style>
