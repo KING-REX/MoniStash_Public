ad93900da0804b6403e5aaffc6c188974d7c5445
KING-REX <anijimjamesmaurice@gmail.com>
2024-04-01 21:15:46 +0100

    Rex's Commit: Adding more generic components

---
+++ src/components/SubscribeNewsletter.vue
@@ -0,0 +1,122 @@
+<template>
+    <section class="call-to-action">
+        <h2>Our Awesome Newsletter</h2>
+        <p>Be the first one to know about discounts, offers and events</p>
+        <form action="">
+            <div class="content">
+                <input type="text" id="subscriptionEmail" placeholder="Enter your email" />
+                <button>Submit</button>
+            </div>
+        </form>
+
+        <div class="vectors">
+            <img src="@/assets/vector1.svg" alt="" />
+            <img src="@/assets/vector2.svg" alt="" />
+            <img src="@/assets/vector3.svg" alt="" />
+        </div>
+    </section>
+</template>
+
+<style scoped>
+/* CALL TO ACTION SECTION STYLES ******************************* */
+
+section.call-to-action {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #25a18e;
+    padding: 50px;
+    margin: 0 30px 50px;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+
+    position: relative;
+    overflow: hidden;
+}
+section.call-to-action h2 {
+    color: #fff;
+    font-size: 1.6rem;
+    margin-bottom: 25px;
+}
+section.call-to-action p {
+    color: #fff;
+    margin-bottom: 30px;
+    font-size: 0.9rem;
+}
+section.call-to-action form .content {
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 8px;
+    border-radius: 8px;
+    background-color: #0f8e7b;
+    width: 450px;
+}
+section.call-to-action form input#subscriptionEmail {
+    border: none;
+    background-color: transparent;
+    color: #fff;
+    padding: 5px;
+}
+section.call-to-action form input#subscriptionEmail:focus {
+    outline: none;
+}
+section.call-to-action form input#subscriptionEmail::placeholder {
+    color: #fff;
+}
+section.call-to-action form button {
+    border: none;
+    background-color: #fff;
+    color: #25a18e;
+    padding: 12px 35px;
+    border-radius: 8px;
+    font-size: 1.05rem;
+}
+section.call-to-action form button:focus {
+    outline: none;
+}
+section.call-to-action .vectors img {
+    position: absolute;
+    pointer-events: none;
+    user-select: none;
+}
+section.call-to-action .vectors img:nth-of-type(1) {
+    top: -30px;
+    left: -30px;
+}
+section.call-to-action .vectors img:nth-of-type(2) {
+    top: -30px;
+    right: -30px;
+}
+section.call-to-action .vectors img:nth-of-type(3) {
+    bottom: -30px;
+    right: -30px;
+}
+
+@media (max-width: 1024px) {
+    section.call-to-action {
+        margin: 0 20px 50px;
+    }
+}
+
+@media (max-width: 768px) {
+    section.call-to-action {
+        padding: 50px 20px;
+    }
+    section.call-to-action p {
+        text-align: center;
+    }
+    section.call-to-action form {
+        align-self: stretch;
+    }
+    section.call-to-action form .content {
+        width: 100%;
+    }
+}
+
+@media (max-width: 425px) {
+    section.call-to-action h2 {
+        font-size: 1.4rem;
+    }
+}
+</style>
---
+++ src/views/sections/blog/BlogNav.vue
@@ -0,0 +1,148 @@
+<script setup lang="ts">
+import IconSearch from '@/components/icons/IconSearch.vue'
+import { onMounted, ref } from 'vue'
+
+const formInput = ref<HTMLInputElement | null>(null)
+const searchForm = ref<HTMLFormElement | null>(null)
+
+onMounted(() => {
+    // const formInput: HTMLInputElement | null = document.querySelector('#searchQueryField')
+    // const searchForm: HTMLFormElement | null = document.querySelector('#searchBlogForm')
+
+    if (formInput.value) {
+        formInput.value.onfocus = function () {
+            searchForm.value && searchForm.value.classList.add('active')
+        }
+        formInput.value.onblur = function () {
+            searchForm.value && searchForm.value.classList.remove('active')
+        }
+    }
+})
+</script>
+
+<template>
+    <nav id="blogNav" class="container">
+        <div class="links">
+            <a href="#" class="link">Cryptocurrency</a>
+            <a href="#" class="link">Digital News</a>
+            <a href="#" class="link">Latest Updates</a>
+            <a href="#" class="link">Assets</a>
+            <a href="#" class="link">Rate</a>
+        </div>
+        <form action="" id="searchBlogForm" ref="searchForm">
+            <div class="form-field">
+                <label for="searchQueryField">
+                    <!-- <img src="images/icons/search.svg" alt="Search icon" /> -->
+                    <IconSearch />
+                </label>
+                <input
+                    type="text"
+                    id="searchQueryField"
+                    placeholder="Search Blog, News, Updates"
+                    ref="formInput"
+                />
+            </div>
+        </form>
+    </nav>
+</template>
+
+<style scoped>
+/* BLOG NAV STYLES ********************************** */
+
+nav#blogNav {
+    padding: 30px 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1.5px solid rgba(0, 0, 0, 0.1);
+}
+nav#blogNav .links {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 30px;
+    width: calc(100% - 250px - 50px);
+}
+nav#blogNav .link {
+    color: #222;
+    font-size: 0.9rem;
+}
+nav#blogNav #searchBlogForm .form-field {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+    background-color: #f0f0f0;
+    border-radius: 8px;
+    width: 250px;
+    padding: 5px 10px;
+}
+nav#blogNav #searchBlogForm .form-field > * {
+    display: block;
+}
+nav#blogNav #searchBlogForm #searchQueryField {
+    border: none;
+    background-color: transparent;
+    width: calc(100% - 39px);
+    padding: 5px;
+}
+nav#blogNav #searchBlogForm #searchQueryField:focus {
+    outline: none;
+}
+nav#blogNav #searchBlogForm #searchQueryField::placeholder {
+    color: #222;
+}
+
+@media (max-width: 1024px) {
+    nav#blogNav {
+        gap: 10px;
+    }
+    nav#blogNav .links {
+        gap: 10px;
+    }
+}
+
+@media (max-width: 768px) {
+    nav#blogNav {
+        flex-direction: column;
+        gap: 20px;
+        padding: 30px 10px;
+        position: relative;
+    }
+    nav#blogNav .links {
+        gap: 20px;
+        overflow-x: auto;
+        width: 100%;
+        padding: 10px 0;
+        padding-right: 54px;
+    }
+    nav#blogNav .link {
+        flex-shrink: 0;
+    }
+
+    nav#blogNav #searchBlogForm {
+        position: absolute;
+        top: 50%;
+        right: 10px;
+        left: calc(100% - 10px - 44px);
+        transform: translateY(-50%);
+        overflow: hidden;
+        transition: left 0.2s;
+    }
+    nav#blogNav #searchBlogForm.active {
+        left: 10px;
+    }
+    nav#blogNav #searchBlogForm .form-field {
+        transition: width 0.2s;
+        width: 100%;
+    }
+    nav#blogNav #searchBlogForm #searchQueryField {
+        padding: 0;
+    }
+    nav#blogNav #searchBlogForm.active #searchQueryField {
+        padding: 5px;
+    }
+}
+
+@media (max-width: 425px) {
+}
+</style>
